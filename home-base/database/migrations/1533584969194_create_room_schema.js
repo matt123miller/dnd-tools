f2dc7ca11e20bad071027025e7796cd2d3b20d9e
Matt Miller <matt123miller@hotmail.co.uk>
2018-08-09 23:51:32 +0100

    Working on a nice rooms migration. Struggling with foreign key somehow.

--- home-base/database/migrations/1533584969194_create_room_schema.js
+++ home-base/database/migrations/1533584969194_create_room_schema.js
@@ -4,17 +4,55 @@ const Schema = use('Schema');
 
 class CreateRoomSchema extends Schema {
     up() {
-        this.create('create_rooms', (table) => {
+        this.create('rooms', (table) => {
             table.increments();
 
-            table.integer('user_id').references('id').inTable('users');
+            table.integer('user_id');
+
+            // table.foreign('user_id').references('id').inTable('users');
+
+            const rooms = [
+                `Alchemist\\'s lab`,
+                'Animal Pen',
+                `Arcanist\\'s Study`,
+                'Armoury',
+                'Bank',
+                'Baths',
+                'Battle Ring',
+                'Boutique',
+                'Caravansary',
+                'Catacombs',
+                'Chapel',
+                'Clock Tower',
+                'Dining Hall',
+                'Docks, Air',
+                'Docks, Water',
+                'Escape Tunnel',
+                'Garden',
+                'Jails',
+                'Library',
+                'Lodgings',
+                'Magical Enchanter',
+                'Market Stalls',
+                'Mill',
+                `Poisoner\\'s Grotto`,
+                'Ritual Circle',
+                'Sewers',
+                'Smithy',
+                'Stables',
+                'Tavern',
+                'Theater',
+                'War Room'
+            ];
+            table.enu('room_type', rooms);
+            table.integer('room_level');
 
             table.timestamps();
         });
     }
 
     down() {
-        this.drop('create_rooms');
+        this.drop('rooms');
     }
 }
 
